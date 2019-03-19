64b3c46f768d5dd900421fe17d92696afcc109a7
Takahiro555555 <tkhr.corei7@gmail.com>
2019-03-20 04:01:50 +0900

    モーター制御クラス仮完成。 実際のゲームパッドからの操作はまだ。 そのため、操作感は分からない。

--- modules/motor_controller.py
+++ modules/motor_controller.py
@@ -14,7 +14,8 @@ ____________________________________
 """"""""""""""""""""""""""""""""""""
 * の状態を前進とする
 '''
-import pigpio
+import pigpio, time
+import numpy as np
 class MotorController(object):
     """docstring for MotorController."""
     def __init__(self):
@@ -23,20 +24,26 @@ class MotorController(object):
         self.__PWM_FREQUENCY = 10000 # 10kHz
         self.__R1_INPUT = 27
         self.__R2_INPUT = 22
-        self.__L1_INPUT = 5
-        self.__L2_INPUT = 6
+        self.__L1_INPUT = 6
+        self.__L2_INPUT = 5
 
         self.__pi = pigpio.pi()
-        output_pin_list = [self.__R_PWM_PIN, self.__L_PWM_PIN, self.__R1_INPUT, self.__R2_INPUT, self.__L1_INPUT, self.__L2_INPUT]
-        for output_pin in output_pin_list:
+        self.__output_pin_list = [self.__R_PWM_PIN, self.__L_PWM_PIN, self.__R1_INPUT, self.__R2_INPUT, self.__L1_INPUT, self.__L2_INPUT]
+        for output_pin in self.__output_pin_list:
             self.__pi.set_mode(output_pin, pigpio.OUTPUT)
 
-        self.__r_before_direction = 0
-        self.__l_before_direction = 0
+        # 超信地旋回するスティックの角度（sin）
+        self.__spin_turn_stick_val_sin = np.sin(np.pi/12) # 弧度法15度
+
+        # モーターの回転方向が変化する操作受付時の時間を入力
+        # 一時停止処理終了時に0にする
+        self.__r_direction_changed_ms = 0
+        self.__l_direction_changed_ms = 0
+
 
     # モーターの出力と回転方向を変更する（pwm）
     # 回転方向逆転時の停止処理は無い
-    # -1.0 <= value_l, value_r <= 1.0
+    # WARNING: -1.0 <= value_l, value_r <= 1.0
     def __operate_motor(self, value_r=0, value_l=0):
         # NOTE: hardware PWM dutycycle 0-1000000
         self.__pi.hardware_PWM(self.__R_PWM_PIN, self.__PWM_FREQUENCY, abs(int(value_r * 1000000)))
@@ -68,6 +75,9 @@ class MotorController(object):
             self.__pi.write(self.__L2_INPUT, 1)
 
 
+    def __calc_radius(self, value_x=0, value_y=0):
+        return np.sqrt(value_x*value_x + value_y*value_y)
+
     def apply_brake(self):
         self.__pi.write(self.__R1_INPUT, 1)
         self.__pi.write(self.__L1_INPUT, 1)
@@ -77,13 +87,18 @@ class MotorController(object):
     # value_x 左右の方向を決定する
     # value_y 前進、後進とモーターの出力を決定する
     def apply_operation(self, value_x=0, value_y=0):
-        self.__operate_motor(value_x, value_y)
-        if(value_y > 0):
-            pass
-        elif(value_x < 0):
-            pass
+        radius = self.__calc_radius(value_x, value_y)
+        stick_val_sin = value_x / radius
+        if stick_val_sin >= self.__spin_turn_stick_val_sin: # 超信地旋回
+            self.__operate_motor(value_x, -1*value_x)
+            return
+        front_or_back = np.sign(value_y)
+        if value_x >= 0:
+            self.__operate_motor(front_or_back*(radius - value_x), front_or_back*radius)
+            return
         else:
-            pass
+            self.__operate_motor(front_or_back*radius, front_or_back*(radius + value_x))
+            return
 
     def __del__(self):
         self.__pi.stop()
