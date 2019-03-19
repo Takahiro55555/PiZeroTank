4af11369171fd50ac1e23c7f8f370234b1d8bdbc
Takahiro555555 <tkhr.corei7@gmail.com>
2019-03-19 18:10:56 +0900

    モータ制御クラスにpigpioを使用。 PWMの独立制御に成功。 RPi.GPIOでPWM独立制御ができなかった原因はライブラリではなく、条件分岐の勘違いだった。

--- modules/motor_controller.py
+++ modules/motor_controller.py
@@ -14,52 +14,65 @@ ____________________________________
 """"""""""""""""""""""""""""""""""""
 * の状態を前進とする
 '''
-import RPi.GPIO as GPIO
+import pigpio
 class MotorController(object):
     """docstring for MotorController."""
     def __init__(self):
-        self.__R_PWM_PIN = 32
-        self.__L_PWM_PIN = 33
-        self.__PWM_FREQUENCY = 1000
-        self.__R1_INPUT = 11
-        self.__R2_INPUT = 13
-        self.__L1_INPUT = 15
-        self.__L2_INPUT = 37
+        self.__R_PWM_PIN = 18
+        self.__L_PWM_PIN = 13
+        self.__PWM_FREQUENCY = 10000 # 10kHz
+        self.__R1_INPUT = 27
+        self.__R2_INPUT = 22
+        self.__L1_INPUT = 5
+        self.__L2_INPUT = 6
 
-        chan_list = [self.__R_PWM_PIN, self.__L_PWM_PIN, self.__R1_INPUT, self.__R2_INPUT, self.__L1_INPUT, self.__L2_INPUT]
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(chan_list, GPIO.OUT)
-        self.__r_pwm = GPIO.PWM(self.__R_PWM_PIN, self.__PWM_FREQUENCY)
-        self.__l_pwm = GPIO.PWM(self.__L_PWM_PIN, self.__PWM_FREQUENCY)
-        self.__r_pwm.start(0)
-        self.__l_pwm.start(0)
+        self.__pi = pigpio.pi()
+        output_pin_list = [self.__R_PWM_PIN, self.__L_PWM_PIN, self.__R1_INPUT, self.__R2_INPUT, self.__L1_INPUT, self.__L2_INPUT]
+        for output_pin in output_pin_list:
+            self.__pi.set_mode(output_pin, pigpio.OUTPUT)
 
         self.__r_before_direction = 0
         self.__l_before_direction = 0
 
     # モーターの出力と回転方向を変更する（pwm）
     # 回転方向逆転時の停止処理は無い
+    # -1.0 <= value_l, value_r <= 1.0
     def __operate_motor(self, value_r=0, value_l=0):
-        # NOTE: 片方のPWMを0にしてしまうと、もう片方の出力も0になってしまうのを回避するため微小値を加算
-        self.__r_pwm.ChangeDutyCycle(abs(value_r))
-        self.__l_pwm.ChangeDutyCycle(abs(value_l))
-        if value_r > 0 and value_l > 0: # 前進
-            GPIO.output([self.__R1_INPUT, self.__L1_INPUT], GPIO.HIGH)
-            GPIO.output([self.__R2_INPUT, self.__L2_INPUT], GPIO.LOW)
-        elif value_r > 0 and value_l < 0: # 左折
-            GPIO.output([self.__R1_INPUT, self.__L2_INPUT], GPIO.HIGH)
-            GPIO.output([self.__R2_INPUT, self.__L1_INPUT], GPIO.LOW)
-        elif value_r < 0 and value_l > 0: # 右折
-            GPIO.output([self.__R2_INPUT, self.__L1_INPUT], GPIO.HIGH)
-            GPIO.output([self.__R1_INPUT, self.__L2_INPUT], GPIO.LOW)
-        elif value_r > 0 and value_l > 0: # 後進
-            GPIO.output([self.__R2_INPUT, self.__L2_INPUT], GPIO.HIGH)
-            GPIO.output([self.__R1_INPUT, self.__L1_INPUT], GPIO.LOW)
-        else: # 停止（非ブレーキ）
-            GPIO.output([self.__R1_INPUT, self.__R2_INPUT, self.__L1_INPUT, self.__L2_INPUT], GPIO.LOW)
+        # NOTE: hardware PWM dutycycle 0-1000000
+        self.__pi.hardware_PWM(self.__R_PWM_PIN, self.__PWM_FREQUENCY, abs(int(value_r * 1000000)))
+        self.__pi.hardware_PWM(self.__L_PWM_PIN, self.__PWM_FREQUENCY, abs(int(value_l * 1000000)))
+        if value_r == 0 and value_l == 0: # 停止（非ブレーキ）
+            self.__pi.write(self.__R1_INPUT, 0)
+            self.__pi.write(self.__L1_INPUT, 0)
+            self.__pi.write(self.__R2_INPUT, 0)
+            self.__pi.write(self.__L2_INPUT, 0)
+        elif value_r >= 0 and value_l >= 0: # 前進
+            self.__pi.write(self.__R1_INPUT, 1)
+            self.__pi.write(self.__L1_INPUT, 1)
+            self.__pi.write(self.__R2_INPUT, 0)
+            self.__pi.write(self.__L2_INPUT, 0)
+        elif value_r >= 0 and value_l <= 0: # 左折
+            self.__pi.write(self.__R1_INPUT, 1)
+            self.__pi.write(self.__L1_INPUT, 0)
+            self.__pi.write(self.__R2_INPUT, 0)
+            self.__pi.write(self.__L2_INPUT, 1)
+        elif value_r <= 0 and value_l >= 0: # 右折
+            self.__pi.write(self.__R1_INPUT, 0)
+            self.__pi.write(self.__L1_INPUT, 1)
+            self.__pi.write(self.__R2_INPUT, 1)
+            self.__pi.write(self.__L2_INPUT, 0)
+        elif value_r >= 0 and value_l >= 0: # 後進
+            self.__pi.write(self.__R1_INPUT, 0)
+            self.__pi.write(self.__L1_INPUT, 0)
+            self.__pi.write(self.__R2_INPUT, 1)
+            self.__pi.write(self.__L2_INPUT, 1)
+
 
     def apply_brake(self):
-        GPIO.output([self.__R1_INPUT, self.__R2_INPUT, self.__L1_INPUT, self.__L2_INPUT], GPIO.HIGH)
+        self.__pi.write(self.__R1_INPUT, 1)
+        self.__pi.write(self.__L1_INPUT, 1)
+        self.__pi.write(self.__R2_INPUT, 1)
+        self.__pi.write(self.__L2_INPUT, 1)
 
     # value_x 左右の方向を決定する
     # value_y 前進、後進とモーターの出力を決定する
@@ -73,6 +86,4 @@ class MotorController(object):
             pass
 
     def __del__(self):
-        self.__r_pwm.stop()
-        self.__l_pwm.stop()
-        GPIO.cleanup()
+        self.__pi.stop()
